013409b0b240115d4cb2ccc027946e402e1834aa
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-02-25 15:33:45 -0500

    added time functionality including display of AM and PM

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var sun = {};
 
 sun.time = function(){
 	var date = new Date();
-	console.log(date);
 	var h = date.getHours()
 	var min = date.getMinutes();
-	var time = h + ":" + min;
+	var ampm = (h >= 12) ? "pm" : "am";
+	h = ((h + 11) % 12 + 1);
+	min = min < 10 ? "0" + min : min;
+	var time = h + ":" + min + " " + ampm;
 	console.log(time);
+	$('.timeDisplay').html(time);
 }
+setInterval(sun.time, 1000);
