7ba8a977725bac46c8b7d1bae23b171f585e5bc1
Jordan Deutsch <jordanbdeutsch@gmail.com>
2015-02-26 09:07:20 -0500

    changed functionality of degree function so that sun is actually moving. currently at weird place in the sky

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 
 var sun = {};
 
+sun.revolve = function(){
+
+};
+//find out the time. 
+//based on the time of day position sun in sky
+// move sun 4.16% every 3600sec (an hour)
+
+
 sun.time = function(){
 	var date = new Date();
-	var h = date.getHours()
-	var min = date.getMinutes();
-	var ampm = (h >= 12) ? "pm" : "am";
-	h = ((h + 11) % 12 + 1);
-	min = min < 10 ? "0" + min : min;
-	var time = h + ":" + min + " " + ampm;
+	sun.h = date.getHours()
+	sun.min = date.getMinutes();
+	var ampm = (sun.h >= 12) ? "pm" : "am";
+	sun.h = ((sun.h + 11) % 12 + 1);
+	sun.min = sun.min < 10 ? "0" + sun.min : sun.min;
+	var time = sun.h + ":" + sun.min + " " + ampm;
 	console.log(time);
 	$('.timeDisplay').html(time);
 }
-setInterval(sun.time, 1000);
+setInterval(sun.time
+	, 1000);
+
+// sun.circle = function(icon, degree){
+// 		 $(icon).attr('transform', 'rotate('+ degree +' 50 50)');
+// 		 console.log(icon)
+// 	}
+
+// sun.circle($('.sun'), 30*(sun.h%12) + sun.min/2);	
+
+
+setInterval(
+	function(){
+		sun.circle = function(icon, degree){
+				icon.css({WebkitTransform: 'rotate('+ (degree + 90) +'deg)'});
+				 console.log(icon)
+			}
+		sun.circle($('.sunrise'), 30*(sun.h%12) + sun.min/2);	
+		console.log(30*(sun.h%12) + sun.min/2);
+	}
+	// function circle(icon, degree){
+	// 	 icon.setAttribute('transform', 'rotate('+ degree +' 50 50)');
+	// 	 console.log('merp')
+	// }
+	// var sunIcon = $('.sun')
+	// var timing = 30*(sun.h%12) + sun.min/2;
+	// circle(sunIcon, timing);
+	// console.log('meep');
+	, 1000)
+
+	
